1faa8521e5e9eb19e58878217292e6197120891a
m29d <1664300699@qq.com>
2026-08-07 22:42:35 +0800

    fix: use git fetch+reset in rebuild.sh to avoid merge conflicts

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 cd /opt/xhblogs-full
-echo '[rebuild] pulling latest code...'
-git pull origin main 2>&1
+echo '[rebuild] fetching latest code...'
+git fetch origin main 2>&1
+git reset --hard origin/main 2>&1
 echo '[rebuild] building...'
 npm run build 2>&1 | tail -5
 echo '[rebuild] copying static files...'
